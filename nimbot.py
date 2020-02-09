5da3cc17270d31ef71069a4a971a4834e07f2ae6
Dave Enrico <denrico@northbergen.k12.nj.us>
2020-02-08 20:31:53 -0500

    Initial commit

---
+++ nimbot.py
@@ -0,0 +1,16 @@
+import os
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='nim')
+async def play_nim(ctx):
+    await ctx.send("Still working on it, kiddo.")
+
+bot.run(token)
+
