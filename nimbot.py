aaf7a500ab9d0be271862f89a32266e4905ba2c9
yelmansoury30 <yelmansoury11@gmail.com>
2020-02-19 15:59:21 +0000

    a commit message (average)

--- nimbot.py
+++ nimbot.py
@@ -12,5 +12,12 @@ bot = commands.Bot(command_prefix='!')
 async def play_nim(ctx):
     await ctx.send("Still working on it, kiddo.")
 
+@bot.command(name='flip_coin')
+async def flip_coin(ctx):
+    await ctx.send(random.choice(["Heads", "Tails"]))
+
+@bot.command(name='average')
+async def average(ctx, a ,b):
+    await ctx.send((a+b)/2)
 bot.run(token)
 
